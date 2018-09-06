b11a9b47b6b885b8a2523c75929126ccd19c3360
Enxebre <alberto.garcial@hotmail.com>
2018-09-06 10:27:53 +0200

    add linting scripts

---
+++ hack/go-fmt.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+if [ "$IS_CONTAINER" != "" ]; then
+  for TARGET in "${@}"; do
+    find "${TARGET}" -name '*.go' ! -path '*/vendor/*' ! -path '*/.build/*' -exec gofmt -s -w {} \+
+  done
+  git diff --exit-code
+else
+  docker run -it --rm \
+    --env IS_CONTAINER=TRUE \
+    --volume "${PWD}:/go/src/github.com/openshift/cluster-api-provider-libvirt:z" \
+    --workdir /go/src/github.com/openshift/cluster-api-provider-libvirt \
+    --entrypoint sh \
+    quay.io/coreos/golang-testing \
+    ./hack/go-fmt.sh "${@}"
+fi
