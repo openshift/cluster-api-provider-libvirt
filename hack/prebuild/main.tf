3ffd4f3dff57694cf0df25ebd89c5dc0c801c49f
Karen Almog <karen.almog@coreos.com>
2018-09-28 17:16:57 +0200

    terraform code for local libvirt testing Remove installation of libvirt-devel from Dockerfile

--- hack/prebuild/main.tf
+++ hack/prebuild/main.tf
@@ -11,19 +11,21 @@ resource "packet_device" "libvirt" {
   billing_cycle    = "hourly"
   project_id       = "${var.packet_project_id}"
   user_data        = "#!/bin/bash\nsed -i 's/PasswordAuthentication.*$/PasswordAuthentication yes/g' /etc/ssh/sshd_config && systemctl restart sshd"
+
   provisioner "remote-exec" {
     script = "init.sh"
+
     connection = {
-      type = "ssh"
-      user = "root"
+      type     = "ssh"
+      user     = "root"
       password = "${self.root_password}"
-      agent = false
+      agent    = false
     }
   }
+
   depends_on = ["packet_ssh_key.key"]
 }
 
 output "ip" {
   value = "${packet_device.libvirt.access_public_ipv4}"
 }
-
