384e5b0a68de25e5bae667ae65825a9b3231cb6e
paulfantom <pawel@krupa.net.pl>
2018-10-16 15:44:07 +0200

    add default storage pool

--- hack/prebuild/init.sh
+++ hack/prebuild/init.sh
@@ -1,4 +1,16 @@
 #/bin/bash
 
+# Create default storage volume
+virsh pool-define /dev/stdin <<EOF
+<pool type='dir'>
+  <name>default</name>
+  <target>
+    <path>/var/lib/libvirt/images</path>
+  </target>
+</pool>
+EOF
+virsh pool-start default
+virsh pool-autostart default
+
 # Start minikube
 /usr/local/bin/minikube start --vm-driver kvm2
