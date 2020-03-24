c7a2d39c9d478859cbd24699f48f5e8c0a6ad55b
Prashanth Sundararaman <psundara@redhat.com>
2020-03-24 10:54:22 -0400

    Use virtio disk instead of config drive for injecting ignition for s390x/ppc64le
    Similar to https://github.com/dmacvicar/terraform-provider-libvirt/pull/718
    
    The method of mimicking what Openstack does for injecting ignition config works for
    images which have the provider as Openstack because ignition recognizes the platform
    and knows it has to get the ignition config from the config drive. For QEMU images, ignition
    supports getting the config from the firmware config device which is not supported by ppc64
    and s390x.
    
    The workaround we have used thus far is to use the Openstack image on the QEMU platform but have this provider create the iso containing the ignition config. There was a discussion in ignition
    (coreos/ignition#928) to have a more QEMU based method of injecting ignition config and it was decided to use a virtio-blk device with a serial of ignition which ignition can recognize. This was mainly because with external devices, it is hard to tell if there is an issue with the device or if the kernel has not detected it yet if it has a long discovery phase.
    
    This PR reverts the ISO method used by Openstack and just creates a virtio-blk device through the QEMU command line options.
    
    Reference PR which supports ignition fetching through virtio-blk for QEMU: coreos/ignition#936

--- pkg/cloud/libvirt/client/domain.go
+++ pkg/cloud/libvirt/client/domain.go
@@ -219,20 +219,48 @@ func newDomainDefForConnection(virConn *libvirt.Connect) (libvirtxml.Domain, err
 	return d, nil
 }
 
-func setCoreOSIgnition(domainDef *libvirtxml.Domain, ignKey string) error {
+func setCoreOSIgnition(domainDef *libvirtxml.Domain, ignKey string, arch string) error {
 	if ignKey == "" {
 		return fmt.Errorf("error setting coreos ignition, ignKey is empty")
 	}
-	domainDef.QEMUCommandline = &libvirtxml.DomainQEMUCommandline{
-		Args: []libvirtxml.DomainQEMUCommandlineArg{
-			{
-				// https://github.com/qemu/qemu/blob/master/docs/specs/fw_cfg.txt
-				Value: "-fw_cfg",
+	if strings.HasPrefix(arch, "s390") || strings.HasPrefix(arch, "ppc64") {
+		// System Z and PowerPC do not support the Firmware Configuration
+		// device. After a discussion about the best way to support a similar
+		// method for qemu in https://github.com/coreos/ignition/issues/928,
+		// decided on creating a virtio-blk device with a serial of ignition
+		// which contains the ignition config and have ignition support for
+		// reading from the device which landed in https://github.com/coreos/ignition/pull/936
+		igndisk := libvirtxml.DomainDisk{
+			Device: "disk",
+			Source: &libvirtxml.DomainDiskSource{
+				File: &libvirtxml.DomainDiskSourceFile{
+					File: ignKey,
+				},
 			},
-			{
-				Value: fmt.Sprintf("name=opt/com.coreos/config,file=%s", ignKey),
+			Target: &libvirtxml.DomainDiskTarget{
+				Dev: "vdb",
+				Bus: "virtio",
 			},
-		},
+			Driver: &libvirtxml.DomainDiskDriver{
+				Name: "qemu",
+				Type: "raw",
+			},
+			ReadOnly: &libvirtxml.DomainDiskReadOnly{},
+			Serial:   "ignition",
+		}
+		domainDef.Devices.Disks = append(domainDef.Devices.Disks, igndisk)
+	} else {
+		domainDef.QEMUCommandline = &libvirtxml.DomainQEMUCommandline{
+			Args: []libvirtxml.DomainQEMUCommandlineArg{
+				{
+					// https://github.com/qemu/qemu/blob/master/docs/specs/fw_cfg.txt
+					Value: "-fw_cfg",
+				},
+				{
+					Value: fmt.Sprintf("name=opt/com.coreos/config,file=%s", ignKey),
+				},
+			},
+		}
 	}
 	return nil
 }
