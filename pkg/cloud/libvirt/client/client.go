adac62c1e8da362122e2076f8e0f4e567c870943
Christophe Fergeau <cfergeau@redhat.com>
2020-07-10 09:55:05 +0200

    Fix a few more virStorageVol leaks

--- pkg/cloud/libvirt/client/client.go
+++ pkg/cloud/libvirt/client/client.go
@@ -195,6 +195,7 @@ func (client *libvirtClient) CreateDomain(input CreateDomainInput) error {
 	if err != nil {
 		return fmt.Errorf("can't retrieve volume %s for pool %s: %v", input.VolumeName, client.poolName, err)
 	}
+	defer diskVolume.Free()
 	if err := setDisks(&domainDef, diskVolume); err != nil {
 		return fmt.Errorf("Failed to setDisks: %s", err)
 	}
@@ -214,6 +215,7 @@ func (client *libvirtClient) CreateDomain(input CreateDomainInput) error {
 		if err != nil {
 			return fmt.Errorf("error getting ignition volume: %v", err)
 		}
+		defer ignVolume.Free()
 		ignVolumePath, err := ignVolume.GetPath()
 		if err != nil {
 			return fmt.Errorf("error getting ignition volume path: %v", err)
@@ -385,6 +387,7 @@ func (client *libvirtClient) CreateVolume(input CreateVolumeInput) error {
 
 	volume, err := client.getVolume(input.VolumeName)
 	if err == nil {
+		volume.Free()
 		return fmt.Errorf("storage volume '%s' already exists", input.VolumeName)
 	}
 
@@ -417,6 +420,7 @@ func (client *libvirtClient) CreateVolume(input CreateVolumeInput) error {
 		if err != nil {
 			return fmt.Errorf("Can't retrieve volume %s", input.BaseVolumeName)
 		}
+		defer baseVolume.Free()
 		var baseVolumeInfo *libvirt.StorageVolInfo
 		baseVolumeInfo, err = baseVolume.GetInfo()
 		if err != nil {
